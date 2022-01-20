7bac86c872704f4d26e5c5e5bf699f4ae3474be4
Michał Okulewicz <m.okulewicz.phd@gmail.com>
2022-01-20 14:34:23 +0100

    Resolves #1 Created StaticDistanceCalculator

---
+++ IO1RouteLib.DistanceProviders/StatictDistanceCalculator.cs
@@ -0,0 +1,17 @@
+﻿namespace IO1RouteLib.DistanceProviders
+{
+    public class StatictDistanceCalculator : IDistanceCalculator
+    {
+        private readonly double _distanceConst;
+
+        public StatictDistanceCalculator(double distanceConst)
+        {
+            _distanceConst = distanceConst;
+        }
+
+        public double GetDistance(Location location1, Location location2)
+        {
+            return _distanceConst;
+        }
+    }
+}
---
+++ IO1RouteLib.Test/StatictDistanceCalculatorTest.cs
@@ -0,0 +1,29 @@
+﻿using IO1RouteLib.DistanceProviders;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IO1RouteLib.Test
+{
+    [TestClass]
+    public class StatictDistanceCalculatorTest
+    {
+        [TestMethod]
+        public void StaticDistanceTest()
+        {
+            double expectedDistance = 2.0;
+            Location location1 = new BaseLocation(0.0, 0.0);
+            Location location2 = new BaseLocation(1.0, 0.0);
+            Location location3 = new BaseLocation(13.0, 15.0);
+
+            IDistanceCalculator staticDistance = new StatictDistanceCalculator(3.0);
+
+            Assert.AreEqual(expectedDistance, staticDistance.GetDistance(location1, location2));
+            Assert.AreEqual(expectedDistance, staticDistance.GetDistance(location2, location3));
+            Assert.AreEqual(expectedDistance, staticDistance.GetDistance(location3, location1));
+        }
+    }
+}
